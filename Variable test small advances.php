6e003425a9653bb6c85545c07c6e8b7294cdeaba
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-02-24 17:08:08 +0530

    Update Variable test small advances.php

--- Variable test small advances.php
+++ Variable test small advances.php
@@ -13,6 +13,7 @@
        $name = 'Aash Gates';
        $age = 25;
        $WHO_IS_HE = 'Friend of John';
+       echo "name\n".$name."and his age\n".$age."So he is\n".$WHO_IS_HE;
     ?>
 </body>
 </html>
