2d2dc189d1711d318eb32e40b03d8eb359fda984
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-02-28 16:55:29 +0530

    Update constant.php

---
+++ constant.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <Title>
+            Arithemetic Operators Test
+        </Title>
+    </head>
+    <body>
+        <?php
+          $num1=8;
+          $num2=6;
+          //Addition
+          echo $num1+$num2."<br>";
+          //subtraction
+          echo $num1-$num2."<br>";
+          //Multiplication
+          echo $num1*$num2."<br>";
+          //Division
+          echo $num1/$num2."<br>";
+          echo "A Demo on the Arithemetic Operators"
+           
+        ?>
+    </body>
+</html>
