53637272236fe4e70ea43e809ac1d19b7827d824
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-03-03 11:00:27 +0530

    Update Numeric Array.php

--- Numeric Array.php
+++ Numeric Array.php
@@ -8,6 +8,7 @@
             $myarray[0]="Aash Gates";
             $myarray[1]="<strong>PHP</strong>";
             $myarray[2]=21;
+            echo "$myarray[0] is $myarray[2] and knows $myarray[1]";
             ?>
         </body>
     </head>
