cb4472c79dfb80f597f72aa6009c5602f9c77d90
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-03-06 13:06:28 +0530

    Update switch.php

--- switch.php
+++ switch.php
@@ -1,51 +1,36 @@
 <html>
-    <head>
-        <title>
-            The Switch Case
-        </title>
-    </head>
-    <body>
-        <?php
-          $today = "Mon";
-            switch($today){
-             case "mon";
-             echo "Today is Monday";
-            break;
-          }
-          switch($today){
-            case "Tue";
-            echo "Today is Tuesday";
-           break;
-          }
+<head>
+    <title>The Switch Case</title>
+</head>
+<body>
+    <?php
+        $today = "Mon";
         switch($today){
-          case "Wed";
-          echo "Today is Wednesday";
-         break;
+            case "Mon":
+                echo "Today is Monday";
+                break;
+            case "Tue":
+                echo "Today is Tuesday";
+                break;
+            case "Wed":
+                echo "Today is Wednesday";
+                break;
+            case "Thu":
+                echo "Today is Thursday";
+                break;
+            case "Fri":
+                echo "Today is Friday";
+                break;
+            case "Sat":
+                echo "Today is Saturday";
+                break;
+            case "Sun":
+                echo "Today is Sunday";
+                break;
+            default:
+                echo "invalid Day";
+                break;
         }
-    switch($today){
-      case "Thu";
-      echo "Today is Thursday";
-     break;
-    }
-switch($today){
-  case "Fri";
-  echo "Today is Friday";
- break;
-}
-
-switch($today){
-  case "Sat";
-  echo "Today is Saturday";
- break;
-}
-
-switch($today){
-  case "Sun";
-  echo "Today is Sunday";
- break;
-}
-  default :
-       echo "invalid Day";
-  ?>
-    </body>
+    ?>
+</body>
 </html>
