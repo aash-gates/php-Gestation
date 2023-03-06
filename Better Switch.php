0c0e461c6406697a1b7ad00ac6621b18a2323877
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-03-06 13:06:24 +0530

    Update Better Switch.php

--- Better Switch.php
+++ Better Switch.php
@@ -13,3 +13,24 @@
                 echo "Today is Tuesday";
                 break;
             case "Wed":
+                echo "Today is Wednesday";
+                break;
+            case "Thu":
+                echo "Today is Thursday";
+                break;
+            case "Fri":
+                echo "Today is Friday";
+                break;
+            case "Sat":
+                echo "Today is Saturday";
+                break;
+            case "Sun":
+                echo "Today is Sunday";
+                break;
+            default:
+                echo "Invalid day";
+                break;
+        }
+    ?>
+</body>
+</html>
